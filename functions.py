5d6c1dde87095cfd9eb826255a02ec5b614d6106
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-20 10:47:41 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin SDK if not already initialized
 if not firebase_admin._apps:
-    cred = credentials.Certificate("coffeeshop-54872-firebase-adminsdk-if85u-f0eabc8124.json")
+    cred = credentials.Certificate('firebase_credentials.json')
     firebase_admin.initialize_app(cred)
 
 # Create Firestore client
