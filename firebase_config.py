d5493dfbd3dde182c8ed405eab8dd7e352b9d8a8
Your Name <you@example.com>
2024-11-21 19:24:32 +0800

    josh .py

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase Admin SDK
+if not firebase_admin._apps:
+    cred = credentials.Certificate(r"firebase_credentials.json")
+    firebase_admin.initialize_app(cred)
+
+# Get Firestore client
+store = firestore.client() 
