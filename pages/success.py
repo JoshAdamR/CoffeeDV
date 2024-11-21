b0bd6d6e5d26e3b909a198a4236c993d8a33c2b4
Your Name <you@example.com>
2024-11-21 15:59:27 +0800

    added cookies

--- pages/success.py
+++ pages/success.py
@@ -2,6 +2,8 @@ import streamlit as st
 from time import sleep
 import streamlit.components.v1 as components
 from navigation import make_sidebar
+from functions import add_entry, get_entries, is_valid_email, is_valid_password, email_exists, fetch_user, cookies, getCookies
+
 
 make_sidebar()
 
@@ -13,3 +15,5 @@ st.markdown("""
 ## Thank you for your payment!
 You can now safely close this page.
 """)
+
+st.write(cookies.getAll())
