ddabd4912a4e9028eaf7d292b1af88068bf7b53d
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-21 16:33:29 +0800

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_credentials)
+    cred = credentials.Certificate(firebase_credentials.json)
     firebase_admin.initialize_app(cred)
 
 # Get Firestore client
