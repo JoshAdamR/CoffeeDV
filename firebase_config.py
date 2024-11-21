a0c2c8d0a9d35dd7086d590b2f3971aa9c147501
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-21 16:35:46 +0800

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,10 +1,26 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import streamlit as st
+
+# Retrieve Firebase credentials from Streamlit secrets
+firebase_secret = st.secrets['firebase']
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
-    cred = credentials.Certificate('firebase_credentials.json')
+    cred = credentials.Certificate({
+        "type": firebase_secret['type'],
+        "project_id": firebase_secret['project_id'],
+        "private_key_id": firebase_secret['private_key_id'],
+        "private_key": firebase_secret['private_key'],
+        "client_email": firebase_secret['client_email'],
+        "client_id": firebase_secret['client_id'],
+        "auth_uri": firebase_secret['auth_uri'],
+        "token_uri": firebase_secret['token_uri'],
+        "auth_provider_x509_cert_url": firebase_secret['auth_provider_x509_cert_url'],
+        "client_x509_cert_url": firebase_secret['client_x509_cert_url'],
+        "universe_domain": firebase_secret['universe_domain']
+    })
     firebase_admin.initialize_app(cred)
 
 # Get Firestore client
-store = firestore.client() 
+store = firestore.client()
