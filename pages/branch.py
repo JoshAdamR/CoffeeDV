ae7bc9abe4c4e7f05157abbf3b0b01159c9464b9
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-25 03:11:16 +0800

    date

--- pages/branch.py
+++ pages/branch.py
@@ -756,22 +756,22 @@ def dashboard():
             # Aggregate profit by day
             sale['date'] = sale['ordered_time_date'].dt.date
             usage_merge['date'] = usage_merge['date'].dt.date
-            revenue_aggregated = sale.groupby('ordered_time_date')['revenue'].sum().reset_index()
+            revenue_aggregated = sale.groupby('date')['revenue'].sum().reset_index()
             cost_aggregated = usage_merge.groupby('date')['cost'].sum().reset_index()            
 
         elif time_period == "Weekly":
             # Aggregate profit by week
             sale['date'] = sale['ordered_time_date'].dt.to_period('W').dt.start_time
             usage_merge['date'] = usage_merge['date'].dt.to_period('W').dt.start_time
-            profit_aggregated = sale.groupby('week')['revenue'].sum().reset_index()
-            cost_aggregated = usage_merge.groupby('week')['cost'].sum().reset_index()
+            profit_aggregated = sale.groupby('date')['revenue'].sum().reset_index()
+            cost_aggregated = usage_merge.groupby('date')['cost'].sum().reset_index()
 
         elif time_period == "Monthly":
             # Aggregate profit by month
             sale['date'] = sale['ordered_time_date'].dt.to_period('M').dt.start_time
             usage_merge['date'] = usage_merge['date'].dt.to_period('M').dt.start_time
-            profit_aggregated = sale.groupby('month')['revenue'].sum().reset_index()
-            cost_aggregated = usage_merge.groupby('month')['cost'].sum().reset_index()
+            profit_aggregated = sale.groupby('date')['revenue'].sum().reset_index()
+            cost_aggregated = usage_merge.groupby('date')['cost'].sum().reset_index()
 
 
         profit_aggregated = pd.merge(revenue_aggregated, cost_aggregated, on='date', how='inner')
