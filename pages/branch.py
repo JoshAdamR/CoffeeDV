e10ddf0184d44f83c2ee02ec40bb615e21c24159
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-24 22:48:03 +0800

    Update branch.py

--- pages/branch.py
+++ pages/branch.py
@@ -457,7 +457,7 @@ def dashboard():
                                                      "Order Monitoring Dashboard"])
         
     # Convert sale_date to datetime if it's not already
-    data['cart']['ordered_time_date'] = pd.to_datetime(data['cart']['ordered_time_date'].tolist)
+    #data['cart']['ordered_time_date'] = pd.to_datetime(data['cart']['ordered_time_date'].tolist)
         
     # Sidebar Filters: Branch, Time Period, and Date Range
     with st.sidebar:
