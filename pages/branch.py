d43239635a1e90f4f7308d4cc4ef2feb0ca65e88
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-26 02:02:46 +0800

    s

--- pages/branch.py
+++ pages/branch.py
@@ -218,9 +218,7 @@ def coupon():
         offer_df_display.index = pd.RangeIndex(start=1, stop=len(offer_df_display) + 1, step=1)
         st.dataframe(offer_df_display, use_container_width = True)  # Display the offers stored in session state
     except:
-        columns_to_display = ['Type', 'Code', 'Start Date', 'Discount', 'Expiry Date']
-        offer_df = offer_df['Type', 'Code', 'Start Date', 'Discount', 'Expiry Date']
-        st.dataframe(offer_df)
+        st.write('No coupon created yet')
 
     # Admin options to add special offers
     with st.form(key='offer_form'):
