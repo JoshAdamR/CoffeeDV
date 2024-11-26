5ea1986ceb98233ce3cea730ddd743a46a74b5de
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-26 15:31:42 +0800

    s

--- pages/branch.py
+++ pages/branch.py
@@ -1075,8 +1075,8 @@ def dashboard():
         st.header("C. Revenue Streams")
         
         # Merge order data with product data on 'product_id'
-        order_data_filtered['product_name'] = order_data_filtered['name']
-        merged_data = pd.merge(order_data_filtered, product_data, on='product_name')
+        order_data_filtered['name'] = order_data_filtered['name']
+        merged_data = pd.merge(order_data_filtered, product_data, on='name')
 
         # Ensure 'total_price' is numeric, coercing errors to NaN
         merged_data['price_after_discount'] = pd.to_numeric(merged_data['price_after_discount'], errors='coerce')
